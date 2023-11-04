0b8ce9f78180ac79283809cb25fd6a952832111b
wvvillafae <126094352+wvvillafae@users.noreply.github.com>
2023-11-04 17:32:48 -0500

    Delete facturacion/facturacion directory

--- facturacion/facturacion/js/compras.js
+++
@@ -1,8 +0,0 @@
-function funcion_calcular(){
-    precio = document.getElementById('precio').value;
-    cantidad = document.getElementById('Cantidad').value;
-    calculo = precio * cantidad;
-    window.alert('El total a pagar es de: ' + calculo);
-    document.getElementById('compra').innerHTML = calculo;
-}
-
